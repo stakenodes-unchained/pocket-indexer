353553498163f566a091c0f9ddc79dbf12c7512a
Muhammad Ehsan <syedehsans1@gmail.com>
2025-08-02 15:32:52 +0500

    query update

--- server/services/transactionService.js
+++ server/services/transactionService.js
@@ -87,7 +87,7 @@ class TransactionService {
       const transactionsResult = await this.pgClient.query(
         `SELECT t.id, t.hash, t.block_id, t.sender, t.recipient, t.amount, t.fee, t.memo, t.type, t.status, t.chain, t.timestamp, b.height as block_height FROM transactions as t
          JOIN blocks as b ON t.block_id = b.id
-         WHERE chain = $1 
+         WHERE b.chain = $1 
          ORDER BY timestamp DESC 
          LIMIT $2 OFFSET $3`,
         [chainName, limitNum, offset]
