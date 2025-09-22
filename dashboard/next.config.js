c2047ad89453bdd1ec777fb400e8bbc09dba1318
Muhammad Ehsan <syedehsans1@gmail.com>
2025-09-22 19:44:14 +0500

    updated the next config so that indexer dashboard can serve on explorer.pocket.network

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone'
+  output: 'standalone',
+  basePath: '/indexer',
+  assetPrefix: '/indexer'
 };
 
 module.exports = nextConfig;
